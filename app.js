84183779ef06c1cc19b65c25813aa2002c503fee
jaesung97 <jaesung4231@naver.com>
2023-05-25 19:17:16 +0900

    Import and render map

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
+
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-console.log(canvas);
+const image = new Image();
+image.src = "./Pellet Town.png";
+console.log(image);
+
+image.onload = () => {
+  c.drawImage(image, -800, -550);
+};
